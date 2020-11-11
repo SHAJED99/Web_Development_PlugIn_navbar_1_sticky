d7557f4b1d83352033f2d8716117f906c86cabc7
SHAJED99 <39647986+SHAJED99@users.noreply.github.com>
2020-11-12 04:21:16 +0600

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,13 +1,16 @@
 function sticky_navbar(header, top_position = 10) {
     var main_class = $(header);
+    main_function();
 
-    $(window).scroll(function() {
+    $(window).scroll(main_function);
+
+    function main_function() {
         var scroll = $(window).scrollTop()
 
         if (scroll > top_position) {
-            main_class.addClass('sticky')
+            main_class.addClass('header_sticky');
         } else {
-            main_class.removeClass('sticky')
+            main_class.removeClass('header_sticky');
         }
-    })
+    }
 }
