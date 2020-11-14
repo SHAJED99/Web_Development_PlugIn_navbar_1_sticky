23ecb638b064b5b58f92870e558a34158264ca86
SHAJED99 <39647986+SHAJED99@users.noreply.github.com>
2020-11-14 17:24:40 +0600

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,9 @@
 function sticky_navbar(header) {
     var main_class = $(header);
-    var distance = (main_class.offset().top - $('body').scrollTop());
+
+
+
+    var distance = (main_class.offset().top - $('body').offset().top);
     main_function();
 
     $(window).scroll(main_function);
@@ -10,7 +13,17 @@ function sticky_navbar(header) {
 
         if (scroll > distance) {
             main_class.addClass('header_sticky');
+            $(header + '.header_sticky').css({
+                'position': 'fixed',
+                'z-index': '9999',
+                'width': '100%',
+            });
         } else {
+            $(header + '.header_sticky').css({
+                'position': 'sticky',
+                'z-index': '9999',
+                'width': 'inherit',
+            })
             main_class.removeClass('header_sticky');
         }
     }
